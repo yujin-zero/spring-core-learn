e011fd70cca0d8eecf53c50b12c2e1a04b7618dd
yujin-zero <poly30@naver.com>
2025-01-12 14:20:09 +0900

    refactor: 테스트 코드에 의존성 주입 방식 적용

--- src/test/java/com/example/core/order/OrderServiceTest.java
+++ src/test/java/com/example/core/order/OrderServiceTest.java
@@ -1,17 +1,24 @@
 package com.example.core.order;
 
-import com.example.core.OrderApp;
+import com.example.core.AppConfig;
 import com.example.core.member.Grade;
 import com.example.core.member.Member;
 import com.example.core.member.MemberService;
-import com.example.core.member.MemberServiceImpl;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder() {
