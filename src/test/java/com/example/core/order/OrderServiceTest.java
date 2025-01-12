2198217191742d01e77480ba37c4a0973f1f882d
yujin-zero <poly30@naver.com>
2025-01-12 10:26:59 +0900

    test: OrderService 테스트 추가

---
+++ src/test/java/com/example/core/order/OrderServiceTest.java
@@ -0,0 +1,25 @@
+package com.example.core.order;
+
+import com.example.core.OrderApp;
+import com.example.core.member.Grade;
+import com.example.core.member.Member;
+import com.example.core.member.MemberService;
+import com.example.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertEquals(order.getDiscountPrice(),1000);
+    }
+}
