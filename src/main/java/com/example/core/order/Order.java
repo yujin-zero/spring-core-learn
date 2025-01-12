1b753575f1328f1d9420b6eb3c3b760aa8e995f4
yujin-zero <poly30@naver.com>
2025-01-12 10:13:12 +0900

    refactor: Order 클래스에 toString 메서드 추가 및 코드 간소화
    - toString 메서드 추가
    - Setter 메서드 제거

--- src/main/java/com/example/core/order/Order.java
+++ src/main/java/com/example/core/order/Order.java
@@ -22,31 +22,25 @@ public class Order {
         return memberId;
     }
 
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
-    }
-
     public String getItemName() {
         return itemName;
-    }
-
-    public void setItemName(String itemName) {
-        this.itemName = itemName;
     }
 
     public int getItemPrice() {
         return itemPrice;
     }
 
-    public void setItemPrice(int itemPrice) {
-        this.itemPrice = itemPrice;
-    }
-
     public int getDiscountPrice() {
         return discountPrice;
     }
 
-    public void setDiscountPrice(int discountPrice) {
-        this.discountPrice = discountPrice;
+    @Override
+    public String toString() {
+        return "Order{" +
+                "memberId= " + memberId +
+                ", itemName= " + itemPrice +
+                ", itemPrice= " + itemPrice +
+                ", discountPrice= " + discountPrice +
+                "}";
     }
 }
