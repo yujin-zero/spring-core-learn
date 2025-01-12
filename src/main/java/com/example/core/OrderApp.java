1813f67fb38cff62a75a80e5abe6264620026dd6
yujin-zero <poly30@naver.com>
2025-01-12 15:41:09 +0900

    feat: Spring @Configuration과 @Bean 적용
    - AppConfig 클래스에 스프링 적용
    - MemberApp, OrderApp에 Spring DI 활용

--- src/main/java/com/example/core/OrderApp.java
+++ src/main/java/com/example/core/OrderApp.java
@@ -5,13 +5,16 @@ import com.example.core.member.Member;
 import com.example.core.member.MemberService;
 import com.example.core.order.Order;
 import com.example.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
