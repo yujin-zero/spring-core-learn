1e9189abd425697a6a183c31f1f3d434bdbcd11d
yujin-zero <poly30@naver.com>
2025-01-14 12:57:05 +0900

    test: 컴포넌트 스캔 기반 자동 빈 등록 테스트 추가

---
+++ src/test/java/com/example/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,19 @@
+package com.example.core.scan;
+
+import static org.assertj.core.api.Assertions.*;
+
+import com.example.core.AutoAppConfig;
+import com.example.core.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AutoAppConfigTest {
+
+    @Test
+    void basicScan() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
