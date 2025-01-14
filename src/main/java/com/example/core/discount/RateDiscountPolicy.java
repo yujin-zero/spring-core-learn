9827b30443834092b539a12ed36c1c40a19a757e
yujin-zero <poly30@naver.com>
2025-01-14 17:11:07 +0900

    feat: 할인 정책 빈 등록 및 @Primary 설정

--- src/main/java/com/example/core/discount/RateDiscountPolicy.java
+++ src/main/java/com/example/core/discount/RateDiscountPolicy.java
@@ -2,9 +2,11 @@ package com.example.core.discount;
 
 import com.example.core.member.Grade;
 import com.example.core.member.Member;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10; // 10% 할인
