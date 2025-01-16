99998f927111ce8bc912c746494852ffd66b9b6b
yujin-zero <poly30@naver.com>
2025-01-16 11:58:45 +0900

    feat: 요청 스코프 MyLogger 추가, 로깅 데모 기능 추가

---
+++ src/main/java/com/example/core/web/LogDemoController.java
@@ -0,0 +1,27 @@
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURI().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
